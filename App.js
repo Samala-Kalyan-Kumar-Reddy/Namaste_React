41364ecf8cc51df008ad64f21b38ba724ad32457
Samala Kalyan Kumar Reddy <82137130+Samala-Kalyan-Kumar-Reddy@users.noreply.github.com>
2023-11-26 21:11:18 -0500

    Update App.js

--- App.js
+++ App.js
@@ -37,19 +37,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 
-const parent = React.createElement("div", { id: "Parent" }, [
-    React.createElement("child", { id: "child1" }, [
-      React.createElement("h1", {}, "I'm a h1 Tag"),
-      React.createElement("h2", {}, "I'm a h2 Tag"),
-    ]),
-    React.createElement("child", { id: "child2" }, [
-      React.createElement("h1", {}, "I'm a h1 Tag"),
-      React.createElement("h2", {}, "I'm a h2 Tag"),
-    ]),
-  ]);
-  const root=ReactDOM.createRoot(document.getElementById("root"));
+// const parent = React.createElement("div", { id: "Parent" }, [
+//     React.createElement("child", { id: "child1" }, [
+//       React.createElement("h1", {}, "I'm a h1 Tag"),
+//       React.createElement("h2", {}, "I'm a h2 Tag"),
+//     ]),
+//     React.createElement("child", { id: "child2" }, [
+//       React.createElement("h1", {}, "I'm a h1 Tag"),
+//       React.createElement("h2", {}, "I'm a h2 Tag"),
+//     ]),
+//   ]);
+//   const root=ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);  
+// root.render(parent);  
 
 //Jsx(transpiled before it reaches the js engine)->pracel-babel
 // Jsx=>React.createElement=>ReactElement=>JS OBJECT=>HTML ELEMENT(RENDER)
